ca0e4ed30da8fe99f54e4f0f9416d587ef8634e5
Tancred423 <yep.tancred+github@gmail.com>
2022-12-05 14:41:28 +0100

    ADD Day05.

---
+++ 2022/Day05/part1.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const input = fs.readFileSync(__dirname + '/input.txt', 'utf8')
+const lines = input.split('\r\n').filter(Boolean)
+
+function parseStacksOfCrates() {
+  const tallestStack = input.match(/^.*\[/gm).length
+  const maxCharacters = lines[tallestStack - 1].length
+  const stacksOfCrates = []
+
+  for (let h = 1; h < maxCharacters; h += 4) {
+    let tmpStack = []
+
+    for (let v = 0; v < tallestStack; v++) {
+      const line = lines[v]
+      tmpStack.push(line.charAt(h))
+    }
+
+    stacksOfCrates.push(
+      tmpStack.reverse().filter((crate) => /[A-Z]/.test(crate))
+    )
+  }
+
+  return stacksOfCrates
+}
+
+function rearrangeStacksOfCrates(stacksOfCrates) {
+  const matches = input.matchAll(
+    /^move (?<move>\d+) from (?<from>\d+) to (?<to>\d+)$/gm
+  )
+
+  for (const match of matches) {
+    const move = match.groups.move
+    const from = match.groups.from
+    const to = match.groups.to
+
+    const cratesToTransfer = stacksOfCrates[from - 1].splice(
+      stacksOfCrates[from - 1].length - move,
+      move
+    )
+
+    stacksOfCrates[to - 1].push(...cratesToTransfer.reverse())
+  }
+}
+
+function getCratesOnTop(stacksOfCrates) {
+  let answer = ''
+
+  for (const stacks of stacksOfCrates) {
+    answer += stacks.pop()
+  }
+
+  return answer
+}
+
+const stacksOfCrates = parseStacksOfCrates()
+rearrangeStacksOfCrates(stacksOfCrates)
+const cratesOnTop = getCratesOnTop(stacksOfCrates)
+
+console.log('Crates on top:', cratesOnTop)
